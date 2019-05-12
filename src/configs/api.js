ea8568926635b0ee74318100fe557fc919ad84ea
liuweitian <916843282@qq.com>
2019-05-12 17:11:27 +0800

    删除apiLocal，若需要请自行创建和实现

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,11 +1,8 @@
 import main from './main';
 import apiProd from './apiProd';
 import apiDev from './apiDev';
-import apiLocal from './apiLocal';
 let api = apiProd;
 if( main.env === 'dev' ) {
     api = apiDev;
-} else if( main.env === 'local' ) {
-    api = apiLocal;
 }
 export default api;
