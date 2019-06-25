7e6dc5dd98f34b8e8af82736890317909c6a15e6
liuweitian <916843282@qq.com>
2019-06-25 09:05:22 +0800

    修正WtPlugins.vue中的组件引用命名规范

--- src/components/WtPlugins.vue
+++ src/components/WtPlugins.vue
@@ -1,10 +1,10 @@
 <template>
     <span v-if="main.ui.plugin === 'element'">
-        <wt-tip></wt-tip>
-        <wt-modal></wt-modal>
-        <wt-message-box></wt-message-box>
-        <wt-notice></wt-notice>
-        <wt-loading></wt-loading>
+        <wt-tip-el></wt-tip-el>
+        <wt-modal-el></wt-modal-el>
+        <wt-message-box-el></wt-message-box-el>
+        <wt-notice-el></wt-notice-el>
+        <wt-loading-el></wt-loading-el>
     </span>
     <span v-else-if="main.ui.plugin === 'yd'">
         <wt-message-box-yd></wt-message-box-yd>
@@ -17,24 +17,24 @@
 
 <script>
     import main from '../configs/main';
-    import WtTip from "./element/WtTip";
-    import WtModal from "./element/WtModal";
-    import WtMessageBox from "./element/WtMessageBox";
-    import WtNotice from "./element/WtNotice";
+    import WtTipEl from "./element/WtTip";
+    import WtModalEl from "./element/WtModal";
+    import WtMessageBoxEl from "./element/WtMessageBox";
+    import WtNoticeEl from "./element/WtNotice";
+    import WtLoadingEl from "./element/WtLoading";
     import WtMessageBoxYd from "./yd/WtMessageBox";
     import WtTipYd from "./yd/WtTip";
     import WtNoticeYd from "./yd/WtNotice";
     import WtModalYd from "./yd/WtModal";
-    import WtLoading from "./element/WtLoading";
     import WtLoadingYd from "./yd/WtLoading";
     export default {
         name: "WtPlugins",
         components: {
-            WtLoading,
-            WtNotice,
-            WtMessageBox,
-            WtModal,
-            WtTip,
+            WtLoadingEl,
+            WtNoticeEl,
+            WtMessageBoxEl,
+            WtModalEl,
+            WtTipEl,
             WtMessageBoxYd,
             WtTipYd,
             WtNoticeYd,
