1abe9c8f713e81504c27b6866cbceee88a620133
liuweitian <xfan1983@msn.com>
2020-08-27 16:46:40 +0800

    默认开启 gz 压缩（需要额外的nginx或者apache配置）

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
-let mode = process.env.NODE_ENV;
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
+let gzipList = [ 'css', 'js' ];
+let mode = process.env.NODE_ENV;
+let fileName = new Date().getTime();
 module.exports = {
     // 构建好的文件输出目录
     publicPath: "/",
     // 构建项目目录
-    outputDir: 'site/' + { production: 'prod', development: 'dev', test: 'test' }[mode] || 'test',
+    outputDir: 'site/' + ( { production: 'prod', development: 'dev', test: 'test' }[mode] || 'test' ) + '/' + fileName,
     // 是否使用带有浏览器内编译器的完整构建版本
     lintOnSave: true,
     // babel-loader默认会跳过`node_modules`依赖
@@ -18,5 +21,21 @@ module.exports = {
         sourceMap: false,
         loaderOptions: {},
         modules: false
+    },
+
+    configureWebpack: {
+        plugins: [
+            new CompressionWebpackPlugin({
+                filename: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: new RegExp('\\.(' + gzipList.join('|') + ')$'),//匹配文件名
+                threshold: 10240,//对10K以上的数据进行压缩
+                minRatio: 0.8,
+                deleteOriginalAssets: false,//是否删除源文件
+            })
+        ]
+    },
+    devServer: {
+        disableHostCheck: true
     },
 };
