74e381ab907db775e8c4285e8241823312c473b4
liuweitian <xfan1983@msn.com>
2020-09-02 15:20:57 +0800

    简化main配置

--- src/configs/main.js
+++ src/configs/main.js
@@ -1,3 +1,5 @@
+import apiConfig from "./api/config";
+
 let mode = process.env.NODE_ENV;
 export default {
     appName: '尾田框架',
@@ -14,44 +16,5 @@ export default {
     },
 
     // 接口配置
-    api: {
-        // 默认解析配置
-        defaultMaps: [
-            {
-                type: 'success',
-                data: {
-                    message: 'data.message'
-                },
-                items: {
-                    'status': 200,
-                    'data.status': 200,
-                }
-            },
-            {
-                type: 'error',
-                data: {
-                    message: 'data.message'
-                },
-                items: {
-                    'status': 200,
-                    'data.status': 400,
-                }
-            },
-        ],
-        // 访问出错的处理函数
-        // 这个时默认处理，如果在get或者post时传递了errorCb（第四个参数），则默认使用自定义处理，否则执行它
-        errorCallBack(response, status) {
-            console.log( response, status );
-        },
-
-        // 授权信息
-        auth: {
-            // 是否自动在header中添加授权校验信息
-            autoAdd: true,
-            // 授权header键名
-            authName: 'Authorization',
-            // 授权值模板，{token}表示具体的token值，将从store.state.user.getAccessToken()获取
-            authTemplate: 'Bearer {token}',
-        },
-    }
+    api: apiConfig
 };
